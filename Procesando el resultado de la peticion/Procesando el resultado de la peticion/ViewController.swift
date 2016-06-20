4b515e65d8e0613b219941b9bbec9708543ea0ff
Timo Siegle <timo.siegle@bechtle.com>
2016-06-20 23:39:46 +0200

    update internet connection check

--- Procesando el resultado de la peticion/Procesando el resultado de la peticion/ViewController.swift
+++ Procesando el resultado de la peticion/Procesando el resultado de la peticion/ViewController.swift
@@ -41,24 +41,7 @@ class ViewController: UIViewController {
             return false
         } else {
             textField.resignFirstResponder()
-            let reachability: Reachability
-            do {
-                reachability = try Reachability.reachabilityForInternetConnection()
-            } catch {
-                return false
-            }
-            
-            if reachability.isReachable() {
-                self.fetchBookDetails(textField.text!)
-            } else {
-                
-                // En caso de falla en Internet, se muestra una alerta indicando ese problema
-                let alertController = UIAlertController(title: "Internet connection", message:
-                    "Please check your connection and try again!", preferredStyle: UIAlertControllerStyle.Alert)
-                alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
-                
-                self.presentViewController(alertController, animated: true, completion: nil)
-            }
+            fetchBookDetails(textField.text!)
             return true
         }
     }
@@ -73,39 +56,48 @@ class ViewController: UIViewController {
         let url = NSURL(string: urls)
         let datos = NSData(contentsOfURL: url!)
         
-        do {
-            let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
-            let dico1 = json as! NSDictionary
-            if dico1.allKeys.count > 0 {
-                let dico2 = dico1["ISBN:\(isbn)"] as! NSDictionary
-                titulo.text = dico2["title"] as! NSString as String
-                
-                if dico2["cover"]?.count > 0 {
-                    let dico3 = dico2["cover"] as! NSDictionary
-                    let portadaString = dico3["large"] as! NSString as String
+        if datos == nil {
+            // En caso de falla en Internet, se muestra una alerta indicando ese problema
+            let alertController = UIAlertController(title: "Internet connection", message:
+                "Please check your connection and try again!", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+        } else {
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                let dico1 = json as! NSDictionary
+                if dico1.allKeys.count > 0 {
+                    let dico2 = dico1["ISBN:\(isbn)"] as! NSDictionary
+                    titulo.text = dico2["title"] as! NSString as String
                     
-                    let portadaUrl = NSURL(string: portadaString)
-                    let portadaData = NSData(contentsOfURL: portadaUrl!)
-                    portada.image = UIImage(data: portadaData!)
-                }
-           
-                var names = ""
-                if dico2["authors"]?.count > 0 {
-                    let dico4 = dico2["authors"] as! NSArray
-                    let firstElem = dico4[0] as! NSDictionary
-                    names = firstElem["name"] as! NSString as String
-                    for item in dico4.dropFirst() {
-                        let obj = item as! NSDictionary
-                        let name = obj["name"] as! NSString as String
-                        names += ", " + name
+                    if dico2["cover"]?.count > 0 {
+                        let dico3 = dico2["cover"] as! NSDictionary
+                        let portadaString = dico3["large"] as! NSString as String
+                        
+                        let portadaUrl = NSURL(string: portadaString)
+                        let portadaData = NSData(contentsOfURL: portadaUrl!)
+                        portada.image = UIImage(data: portadaData!)
                     }
-                } else {
-                    names = "-"
+                    
+                    var names = ""
+                    if dico2["authors"]?.count > 0 {
+                        let dico4 = dico2["authors"] as! NSArray
+                        let firstElem = dico4[0] as! NSDictionary
+                        names = firstElem["name"] as! NSString as String
+                        for item in dico4.dropFirst() {
+                            let obj = item as! NSDictionary
+                            let name = obj["name"] as! NSString as String
+                            names += ", " + name
+                        }
+                    } else {
+                        names = "-"
+                    }
+                    autores.text = names
                 }
-                autores.text = names
+            } catch {
+                print("Error info: \(error)")
             }
-        } catch {
-            print("Error info: \(error)")
         }
     }
 }
